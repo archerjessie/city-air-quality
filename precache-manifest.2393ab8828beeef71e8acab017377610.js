e7e07a23caa96e95e46bead268fc383da1d8733c
archerjessie <66236559+archerjessie@users.noreply.github.com>
2020-10-10 22:58:45 +1100

    Updates

---
+++ precache-manifest.2393ab8828beeef71e8acab017377610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358aeb4c6094587c77136591e7bea749",
+    "url": "/city-air-quality/index.html"
+  },
+  {
+    "revision": "9517305a34daca35c95f",
+    "url": "/city-air-quality/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2a5e4091dc6af2d26fb9",
+    "url": "/city-air-quality/static/css/main.dd90b70d.chunk.css"
+  },
+  {
+    "revision": "9517305a34daca35c95f",
+    "url": "/city-air-quality/static/js/2.fd96fa10.chunk.js"
+  },
+  {
+    "revision": "2a5e4091dc6af2d26fb9",
+    "url": "/city-air-quality/static/js/main.64396877.chunk.js"
+  },
+  {
+    "revision": "279c1dfa1423d74a555d",
+    "url": "/city-air-quality/static/js/runtime-main.6882987e.js"
+  }
+]);
